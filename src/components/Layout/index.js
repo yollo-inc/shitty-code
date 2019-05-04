aa78d7b7024e4c2956eb996eaca05911bb1dfe5b
DaiAoki <a.dai.0814ap@gmail.com>
2019-05-04 17:46:37 +0900

    complete to implement Layout.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+import Header from './Header'
 
+const Container = styled.div`
+`
+const Main = styled.main`
+  padding: 0 18px;
+  max-width: 597px;
+  min-width: 320px;
+  margin: 72px auto 0;
+`
 class Layout extends React.Component {
 
   render() {
     return (
-      <div>
-        { this.props.children }
-      </div>
+      <Container>
+        <Header/>
+        <Main>
+          { this.props.children }
+        </Main>
+      </Container>
     )
   }
 }
